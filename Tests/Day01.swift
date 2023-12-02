e754772c13386586a64faa1ad2d70aa1c29a8b24
Sean Kelly <seankelly.iosdev@gmail.com>
2023-12-01 19:37:33 -0800

    Day01 tests

---
+++ Tests/Day01.swift
@@ -0,0 +1,39 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sean Kelly on 12/1/23.
+//
+
+import XCTest
+
+@testable import AdventOfCode
+
+final class Day01Tests: XCTestCase {
+  let testDataPart1 = """
+    1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet
+    """
+    
+  let testDataPart2 = """
+    two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen
+    """
+
+  func testPart1() throws {
+    let challenge = Day01(data: testDataPart1)
+    XCTAssertEqual(String(describing: challenge.part1()), "142")
+  }
+
+  func testPart2() throws {
+    let challenge = Day01(data: testDataPart2)
+    XCTAssertEqual(String(describing: challenge.part2()), "281")
+  }
+}
