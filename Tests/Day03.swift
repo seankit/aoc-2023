22e9d53b3570fe4b5f191657ff4701039fc8a8ff
Sean Kelly <seankelly.iosdev@gmail.com>
2023-12-03 10:19:35 -0800

    Setup and tests for Day03

---
+++ Tests/Day03.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sean Kelly on 12/3/23.
+//
+
+import Foundation
+import XCTest
+@testable import AdventOfCode
+
+final class Day03Test: XCTestCase {
+    
+    let testData = """
+        467..114..
+        ...*......
+        ..35..633.
+        ......#...
+        617*......
+        .....+.58.
+        ..592.....
+        ......755.
+        ...$.*....
+        .664.598..
+        """
+    
+    func testPart1() {
+        let challenge = Day03(data: testData)
+        XCTAssertEqual(String(describing: challenge.part1()), "4361")
+    }
+    
+    func testPart2() {
+        let challenge = Day03(data: testData)
+        XCTAssertEqual(String(describing: challenge.part2()), "467835")
+    }
+}
