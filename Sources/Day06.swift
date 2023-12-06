894619297ae1ad939a5e8b82720d2d38902f8c54
Sean Kelly <seankelly.iosdev@gmail.com>
2023-12-06 08:39:39 -0800

    refactor duplicated code

--- Sources/Day06.swift
+++ Sources/Day06.swift
@@ -9,43 +9,35 @@ import Foundation
 
 struct Day06: AdventDay {
     let data: String
+    var lines: [Substring] {
+        data.split(whereSeparator: \.isNewline)
+    }
 
     func part1() -> Any {
-        let lines = data.split(whereSeparator: \.isNewline)
         let values = lines.compactMap { $0.split(separator: ":").last }.map { $0.split{ $0 == " " }.compactMap { Int($0) } }
         let times = values[0]
         let distances = values[1]
         
-        var counts: [Int] = []
-        for (index, time) in times.enumerated() {
-            let distance = distances[index]
-            var counter = time
-            var options = 0
-            while counter >= 0 {
-                if counter * (time - counter) > distance {
-                    options += 1
-                }
-                counter -= 1
-            }
-            counts.append(options)
-        }
-        return counts.reduce(1, *)
+        return zip(times, distances).map { calculate(time: $0, distance: $1) }.reduce(1, *)
     }
     
     func part2() -> Any {
-        let lines = data.split(whereSeparator: \.isNewline)
         let values = lines.compactMap { $0.split(separator: ":").last?.filter { !$0.isWhitespace } }.compactMap { Int($0) }
         let time = values[0]
         let distance = values[1]
         
+        return calculate(time: time, distance: distance)
+    }
+    
+    func calculate(time: Int, distance: Int) -> Int {
         var counter = time
-        var options = 0
+        var wins = 0
         while counter >= 0 {
             if counter * (time - counter) > distance {
-                options += 1
+                wins += 1
             }
             counter -= 1
         }
-        return options
+        return wins
     }
 }
